95a5abad0f9b61cd856a8aafec17f5a3a7dc6614
saraybakker1 <saraybakker@gmail.com>
2024-09-24 16:53:04 +0200

    sh files for installing all required packages on local workspace

---
+++ install_packages/install_packages.sh
@@ -0,0 +1,3 @@
+cd ../..
+git clone --branch v1.0.0 git@github.com:INTERACT-tud-amr/dinova_task_planner.git
+git clone --branch v1.0.0 git@github.com:INTERACT-tud-amr/visualization_utils.git
---
+++ install_packages/install_packages_complete.sh
@@ -0,0 +1,7 @@
+cd ../..
+git clone --branch v1.0.0 git@github.com:INTERACT-tud-amr/dinova.git
+git clone --branch v1.0.0 git@github.com:INTERACT-tud-amr/dinova_utils.git
+git clone --branch v1.0.0 git@github.com:INTERACT-tud-amr/dinova_motion.git
+git clone --branch v1.0.0 git@github.com:INTERACT-tud-amr/dinova_grasp.git
+git clone --branch v1.0.0 git@github.com:INTERACT-tud-amr/dinova_task_planner.git
+git clone --branch v1.0.0 git@github.com:INTERACT-tud-amr/visualization_utils.git
